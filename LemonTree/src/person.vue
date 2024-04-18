cf78121f42591e2356ca7257a04d8d979ecb4e3d
HengyanSuper <167294453+HengyanSuper@users.noreply.github.com>
2024-04-18 15:54:25 +0800

    Create person.vue

---
+++ LemonTree/src/person.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="box">
+
+    </div>
+</template>
+
+<script lang="ts">
+export default {
+    name: 'person',
+    setup() {
+
+    }
+
+
+
+}
+
+</script>
+
+<style>
+.box{
+    background-color: aliceblue;
+    border-radius: 20px;
+
+}
+
+
+
+
+</style>
